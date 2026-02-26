5a260d0680226ba67676081c7f5aa3171094c3c9
Kurd123789 <124293267+Kurd123789@users.noreply.github.com>
2026-02-26 23:52:54 +0300

    Implement gold price retrieval and messaging
    Added functionality to fetch and send silver price updates using the Gold API.

--- bot.py
+++ bot.py
@@ -1,3 +1,28 @@
 import os
+import requests
+from telegram import Bot
 
-print("Bot started successfully ✅")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+GOLD_API = os.getenv("GOLD_API")
+
+bot = Bot(token=BOT_TOKEN)
+
+def get_gold_price():
+    url = f"https://www.goldapi.io/api/XAG/USD"
+    headers = {
+        "x-access-token": GOLD_API,
+        "Content-Type": "application/json"
+    }
+    response = requests.get(url, headers=headers)
+    data = response.json()
+    return data["price"]
+
+def send_price():
+    price = get_gold_price()
+    message = f"📊 Silver Price Update\n\n💰 Price: ${price} per ounce"
+    
+    # 👇 ئێرە CHAT_ID دابنێ
+    bot.send_message(chat_id="@xkalola", text=message)
+
+if __name__ == "__main__":
+    send_price()
